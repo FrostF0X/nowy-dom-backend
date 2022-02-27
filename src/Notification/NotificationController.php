0957ec2b5b0c37f3c9d2562c609b52fce7aaf47b
frost_fox <dmytro.konovalov@daftcode.pl>
2022-02-27 13:16:24 +0100

    add cache

--- src/Notification/NotificationController.php
+++ src/Notification/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Notification;
 use App\Common\Controller\ControllerInterface;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
+use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class NotificationController implements ControllerInterface
@@ -22,21 +23,34 @@ class NotificationController implements ControllerInterface
             $region = null;
         }
         if ($region && !NotificationRegion::accepts($region)) {
-            return new JsonResponse('Cannot accept value ' . $region, 400);
+            return $this->response('Cannot accept value ' . $region, 400);
         }
         $offers = $region ? $this->notifications->findBy(['region' => NotificationRegion::get($region)]) : $this->notifications->findAll();
-        return new JsonResponse(NotificationOutput::createMany(...$offers));
+        return $this->response(NotificationOutput::createMany(...$offers));
+    }
+
+    private function response(array|string $res, ?int $status = 200): JsonResponse
+    {
+        return new JsonResponse(
+            $res,
+            $status,
+            $this->cache()
+        );
+    }
+
+    #[ArrayShape(['cache-control' => "string"])] private function cache(): array
+    {
+        return ['cache-control' => 'max-age=60'];
     }
 
     #[Get('/api/v1/notification/regions')]
     public function regions(): JsonResponse
     {
-        return new JsonResponse(
-            collect(NotificationRegion::instances())
-                ->keyBy(fn(NotificationRegion $region) => $region->getValue())
-                ->map(fn(NotificationRegion $region) => $region->getReadable())
-                ->all()
-        );
+        $res = collect(NotificationRegion::instances())
+            ->keyBy(fn(NotificationRegion $region) => $region->getValue())
+            ->map(fn(NotificationRegion $region) => $region->getReadable())
+            ->all();
+        return $this->response($res);
     }
 
 
